7f71e54fa7f1dfcf4bf5d3165291a66691ac8a2a
Vickopiyo <victor.okoth@student.moringaschool.com>
2022-06-02 23:06:41 +0300

    Done.

--- index.js
+++ index.js
@@ -1,2 +1,9 @@
-function addingEventListener() {
+const sinon = document.getElementById("input")
+
+
+
+function addingEventListener() { 
+    sinon.addEventListener("click",function(event){
+        return"clicked"
+    })
 }
